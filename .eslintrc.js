97be9e6770745790827c16fcf1ab55e445a26f98
Alexei <hello@alexei.ro>
2017-05-06 18:32:49 +0300

    Introduce ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    env: {
+        node: true,
+        es6: true
+    },
+    rules: {
+        'indent': ['error', 4],
+        'quote-props': ['error', 'as-needed'],
+        'no-cond-assign': 0,
+        'no-console': 0,
+        'no-control-regex': 0,
+        'no-undef': 0,
+        'no-unused-vars': 0
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ const gulp = require('gulp')
 const babel = require('gulp-babel')
 const mocha = require('gulp-mocha')
 const umd = require('gulp-umd')
+const eslint = require('gulp-eslint')
 
-gulp.task('build', function() {
+gulp.task('lint', function() {
+    return gulp
+        .src('index.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+})
+
+gulp.task('build', ['lint'], function() {
     return gulp
         .src('index.js')
         .pipe(babel({presets: ['es2015']}))
